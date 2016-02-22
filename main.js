d6397ff8c05c08d1a6439460b53b142c6649f1fb
Akshay Thakare <akshay@thakare.xyz>
2016-02-22 23:45:14 +0530

    added webpage and new modules

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
 'use strict';
 
+//Set up express framework
+var app = require('express')();
+
+//Set port given by heroku else set default port 3000
+app.set('port', (process.env.PORT || 3000));
+
+//Create http server - express 4 no longer supports 
+//server creation
+var http = require('http').createServer(app);
+
+//Set http server to listen on this port
+http.listen(app.get('port'), function(){
+	console.log('listening on *:' + app.get('port'));
+});
+
+//Attach socket.io to the http server
+var io = require('socket.io')(http);
+
+//Set default get path / home path
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.get('transfer.html', function(req, res){
+	res.sendFile(__dirname + '/transfer.html');
+});
+
+//XHR polling doesnt seem to work with heroku :|
+//io.set('transports', ['xhr-polling']);
+
+// Transmission stuff -------------------------------
+
 var Transmission = require('transmission');
 var transmission = new Transmission({
 	port: 9091,
-	host: '192.168.1.69',
+	host: '127.0.0.1',
 	username: 'rambo',
 	password: 'qwerty'
 });
@@ -16,33 +48,6 @@ function getStats(){
 		console.log(result);
 	}
 	});
-}
-
-function deleteTorrent(id){
-	transmission.remove(id, true, function(err, result){
-		if (err){
-			console.log(err);
-		} else {
-			console.log(result);
-		}
-	});
-}
-
-function addTorrent(url){
-	transmission.addUrl(url, {
-	    "download-dir" : "~/transmission/torrents"
-	}, function(err, result) {
-	    if (err) {
-	        return console.log(err);
-	    }
-	    var id = result.id;
-	    console.log('Just added a new torrent.');
-	    console.log('Torrent ID: ' + id);
-	});
-}
-
-function startTorrent(id){
-	transmission.start(id, function(err, result){});
 }
 
 function getAllActiveTorrents(){
@@ -59,25 +64,17 @@ function getAllActiveTorrents(){
 	});
 }
 
-function stopAllActiveTorrents(){
-	transmission.active(function(err, result){
-	if (err){
-		console.log(err);
-	}
-	else {
-		for (var i=0; i< result.torrents.length; i++){
-			stopTorrents(result.torrents[i].id);
-		}
-	}
+function addTorrent(url){
+	transmission.addUrl(url, {
+	    "download-dir" : "~/transmission/torrents"
+	}, function(err, result) {
+	    if (err) {
+	        return console.log(err);
+	    }
+	    var id = result.id;
+	    console.log('Just added a new torrent.');
+	    console.log('Torrent ID: ' + id);
 	});
-}
-
-function stopTorrent(id){
-	transmission.stop(id, function(err, result){});
-}
-
-for(var i=0;i<10;i++){
-	getTorrentDetails(i);
 }
 
 function getTorrentDetails(id) {
@@ -86,7 +83,6 @@ function getTorrentDetails(id) {
             throw err;
         }
         if(result.torrents.length > 0){
-        	// console.log(result.torrents[0]);
         	console.log("Name = "+ result.torrents[0].name);
         	console.log("Download Rate = "+ result.torrents[0].rateDownload/1000);
         	console.log("Upload Rate = "+ result.torrents[0].rateUpload/1000);
@@ -96,6 +92,66 @@ function getTorrentDetails(id) {
         }
     });
 }
+
+function stopTorrent(id){
+	transmission.stop(id, function(err, result){});
+}
+
+function removeTorrent(id) {
+    transmission.remove(id, function(err) {
+        if (err) {
+            throw err;
+        }
+        console.log('torrent was removed');
+    });
+}
+
+// End Transmission stuff -------------------------------
+
+//Socket.io listners
+io.sockets.on('connection', function(socket){
+	console.log('a user connected');
+	socket.on('disconnect',function(){
+		console.log('a user disconnected');
+	});
+
+	socket.on('addLink', function(link){
+		console.log(link);
+	});
+
+	// socket.on('chat message',function(msg){
+	// 	console.log('message: '+msg);
+	// 	io.emit('chat message',msg);
+	// });
+
+	socket.on('getTransferList', function(msg){
+		i=96;
+		var timeoutId = setInterval(function(){
+			if(i>100){
+				clearInterval(timeoutId);
+			} else {
+				var data = {
+					name : 'test',
+					val : i,
+					id : 123,
+					down : i,
+					up : i,
+					status : 'Downloading',
+					eta : (100-i)+' hr'
+				};
+				i++;
+
+				socket.emit('transferList',JSON.stringify(data));
+			}
+		},1000);
+	});
+});
+
+
+
+
+
+
 
 function getStatusType(type){
 	if(type === 0){
@@ -115,13 +171,4 @@ function getStatusType(type){
 	} else if(type === 7){
 		return 'ISOLATED';
 	}
-}
-
-function removeTorrent(id) {
-    transmission.remove(id, function(err) {
-        if (err) {
-            throw err;
-        }
-        console.log('torrent was removed');
-    });
 }
